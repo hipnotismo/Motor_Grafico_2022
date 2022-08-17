732a8814135dd5fad6fbafe3182a26e570e9cbaf
martin <martinconcetti97@gmail.com>
2022-08-17 01:11:06 -0300

    Base Game

---
+++ GraphicsEngine/EngineDll/BaseGame.cpp
@@ -0,0 +1,60 @@
+#include "BaseGame.h"
+#include <glfw3.h>
+BaseGame::BaseGame()
+{
+	window = new Window();
+	renderer = new Renderer();
+}
+BaseGame::~BaseGame()
+{
+	if (window != NULL) delete window;
+	if (renderer != NULL) delete renderer;
+}
+int BaseGame::EngineInit(int width, int height, const char* title)
+{
+	if (!glfwInit())
+		return -1;
+	else if (WindowInit(width, height, title))
+		return -1;
+	return 0;
+}
+
+int BaseGame::WindowInit(int width, int height, const char* title)
+{
+	window = new Window(width, height, title, NULL, NULL);
+	if (!window)
+	{
+		return -1;
+	}
+	window->WindowContext();
+	return 0;
+}
+bool BaseGame::Running()
+{
+	return window->ShouldClose();
+}
+
+void BaseGame::UpdateBegin()
+{
+	renderer->BeginDraw();
+}
+
+void BaseGame::UpdateEnd()
+{
+	renderer->SwapBuffer(window->GetWindow());
+	glfwPollEvents();
+}
+void BaseGame::UpdateEngine()
+{
+	while (Running())
+	{
+		UpdateBegin();
+		Update();
+		UpdateEnd();
+	}
+	EngineEnd();
+}
+void BaseGame::EngineEnd()
+{
+	glfwTerminate();
+}
--- GraphicsEngine/EngineDll/BaseGame.h
+++ GraphicsEngine/EngineDll/BaseGame.h
@@ -1,1 +1,25 @@
-#pragma once
+#ifndef BASEGAME_H
+#define BASEGAME_H
+#include "Renderer.h"
+struct GLFWmonitor;
+struct GLFWwindow;
+class BaseGame
+{
+private:
+	Window* window;
+	Renderer* renderer;
+	void UpdateBegin();
+	void UpdateEnd();
+	void EngineEnd();
+public:
+	BaseGame();
+	~BaseGame();
+	int EngineInit(int width, int height, const char* title);
+	int WindowInit(int width, int height, const char* title);
+
+	bool Running();
+	void UpdateEngine();
+	void virtual Update() = 0;
+	void virtual Draw() = 0;
+};
+#endif
