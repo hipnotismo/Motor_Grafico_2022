802a8981b0531a9ea14d05a4499ac6276a84a833
martin <martinconcetti97@gmail.com>
2022-08-31 07:34:04 -0300

    Render triangle

--- GraphicsEngine/EngineDll/Src/Renderer/Renderer.cpp
+++ GraphicsEngine/EngineDll/Src/Renderer/Renderer.cpp
@@ -1,16 +1,98 @@
 #include "renderer.h"
+#include <glew.h>
 #include <glfw3.h>
+#include <iostream>
+#include <fstream>
+#include <sstream>
 
 Renderer::Renderer()
 {
 }
 
-void Renderer::BeginDraw()
+void Renderer::Draw()
 {
-	glClear(GL_COLOR_BUFFER_BIT);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(float) * 18, VertexTriangle, GL_STATIC_DRAW);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(float) * 3, IndexTriangle, GL_STATIC_DRAW);
+	glDrawElements(GL_TRIANGLES, 3, GL_UNSIGNED_INT, 0);
+
 }
 
-void Renderer::SwapBuffer(GLFWwindow* window)
+void Renderer::CreateBuffers()
 {
-	glfwSwapBuffers(window);
+	glGenVertexArrays(1, &VAO); // first: Specifies the number of vertex array object
+								// second: the generated vertex array object names are stored
+	glGenBuffers(1, &VBO); //first: the number of buffer object
+	glGenBuffers(1, &EBO); //second: an array in which the generated buffer object names are stored
+}
+
+void Renderer::BindBuffers()
+{
+	glBindVertexArray(VAO);// Specifies the name of the vertex array to bind
+	glBindBuffer(GL_ARRAY_BUFFER, VBO); //first: type buffer to bound
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO); //second: the buffer
+}
+void Renderer::DefVertexAttribute()
+{
+	//first: defPosMemoryShader(Layout),second:countData, three: TypeDataAttribute, four: Normalize
+	//five: countDataForVertex * SizeOf(typeData), six:StartCount (void*)0) the first point in data to count
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(0); //activate first: defPosMemoryShader(Layout)
+
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
+	glEnableVertexAttribArray(1);
+}
+
+
+unsigned int Renderer::CompileShader(unsigned int type, const char* shaderPath) { //first: ShaderType(Fragment, vertex)
+																		//second:Dir to archive
+	unsigned int id = glCreateShader(type); // Create Shader
+
+	std::string sourceShaderCode; //store source archive
+
+	std::ifstream sourceShaderFile; // interact to archive
+
+	sourceShaderFile.exceptions(std::ifstream::failbit | std::ifstream::badbit);
+
+	try {
+		sourceShaderFile.open(shaderPath);
+		std::stringstream shaderStream;
+
+		shaderStream << sourceShaderFile.rdbuf(); //conversion to StreamString
+
+		sourceShaderFile.close();
+
+		sourceShaderCode = shaderStream.str(); //conversion to string
+	}
+	catch (std::ifstream::failure& e) {
+		std::cout << "ERROR::SHADER::FILE_NOT_SUCCESFULLY_READ" << std::endl;
+	}
+
+	const char* srcCode = sourceShaderCode.c_str(); // conversion to char
+
+	glShaderSource(id, 1, &srcCode, nullptr); //Set source to Shader
+											// First: Shader, Second: conunt elements in the string
+											// three: Specifies an array of pointers to strings containing the source
+											// four: Specifies an array of string lengths
+	glCompileShader(id); //Complie Shader
+
+	return id;
+}
+
+void Renderer::CreateProgram(const char* vertexShaderPath, const char* fragmentShaderPath) {
+	//first: vertex archive
+	//second: fragmentShader
+	program = glCreateProgram(); // create program
+	unsigned int vertex = CompileShader(GL_VERTEX_SHADER, vertexShaderPath);
+	unsigned int fragment = CompileShader(GL_FRAGMENT_SHADER, fragmentShaderPath);
+
+	glAttachShader(program, vertex); //attach with program
+	glAttachShader(program, fragment); //attach with program
+	glLinkProgram(program); // Link with OpenGL
+	glValidateProgram(program); //validate
+	glUseProgram(program);
+
+	glDetachShader(program, vertex); //UnAttach
+	glDetachShader(program, fragment);//UnAttach
+	glDeleteShader(vertex); //delete
+	glDeleteShader(fragment); //delete
 }
--- GraphicsEngine/EngineDll/Src/Renderer/Renderer.h
+++ GraphicsEngine/EngineDll/Src/Renderer/Renderer.h
@@ -1,16 +1,34 @@
 #ifndef RENDERER_H
 #define RENDERER_H
-#include "Window.h"
-struct GLFWwindow;
+#include "export.h"
 class Renderer
 {
 private:
 public:
 	Renderer();
-	void BeginDraw();
-	void SwapBuffer(GLFWwindow* window);
+	void Draw();
+	unsigned int VBO = 0; // VertexBufferObject
+	unsigned int VAO = 0; // VertexArrayObject
+	unsigned int EBO = 0; // ElementsBufferObject
+	unsigned int program = 0; // ProgramShader
+	void CreateBuffers();
+	void BindBuffers();
+	unsigned int CompileShader(unsigned int type, const char* shaderPath);
+	void CreateProgram(const char* vertexShaderPath, const char* pixelShaderPath);
+	void DefVertexAttribute();
 
+	float VertexTriangle[18] = {
+	-0.5f, -0.5f, 0.0f,   1.0f, 0.0f, 1.0f,// X,Y,Z  R,G,B
+	 0.5f, -0.5f, 0.0f,   0.0f, 1.0f, 1.0f,
+	 0.0f,  0.5f, 0.0f,   1.0f, 1.0f, 0.0f
+	};
+
+	unsigned int IndexTriangle[3] = {
+		0, 1, 2
+	};
 };
-
+// settings
+//const unsigned int SCR_WIDTH = 800;
+//const unsigned int SCR_HEIGHT = 600;
 
 #endif
